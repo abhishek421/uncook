b3a43689bd80e51e4e45950f2ea57419cd357d1b
Abhishek Choudhary <64063908+abhishek421@users.noreply.github.com>
2020-08-20 08:36:20 +0530

    1.0

---
+++ components/landing.js
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import styles from '../styles/Landing.module.css'
+
+class Landing extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    
+    handleSubmit(event) {
+        alert('Ohooo.. Bade aaye ' + this.state.value + ' mangane wale.. Are paaji thand rkho..!!');
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <div className={styles.cover}>
+            <div className={styles.brand}>
+                <p style={{fontWeight:"600",fontSize:"60px",color:"white",margin:"auto"}}>UnCook</p>
+                <p style={{fontWeight:"400",fontSize:"20px",color:"white",margin:"auto"}}>"Homemade Happiness"</p>
+            </div>
+            <div className={styles.tagline}>
+                <p>Instant ready to be cooked food in minutes</p>
+            </div>
+            <form onSubmit={this.handleSubmit} className={styles.searchbar} >
+                <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Search Dishes, Cuisines, Restaurants.." className={styles.search} />
+                <button type="submit" className={styles.submit}><img src="./assets/search.svg"/></button>
+            </form>
+            </div>
+        )
+    }
+}
+
+export default Landing
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,20 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Navbar from '../components/navbar';
+import Landing from '../components/landing';
 
 class Home extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('Ohooo.. Bade aaye ' + this.state.value + ' mangane wale.. Are paaji thand rkho..!!');
-    event.preventDefault();
-  }
-
   render(){
       return(
-        <div className={styles.bgcontainer}>
+        <div className={styles.container}>
         <Head>
           <title>UnCook</title>
           <link rel="icon" href="/logo-r.svg" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
         </Head>
         <Navbar />
-        <div className={styles.brand}>
-          <p style={{fontWeight:"600",fontSize:"60px",color:"white",margin:"auto"}}>UnCook</p>
-          <p style={{fontWeight:"400",fontSize:"20px",color:"white",margin:"auto"}}>"Homemade Happiness"</p>
-        </div>
-        <div className={styles.tagline}>
-          <p>Instant ready to be cooked food in minutes</p>
-        </div>
-        <form onSubmit={this.handleSubmit} className={styles.searchbar} >
-        <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Search Dishes.." className={styles.search} />
-        <input type="submit" value='Search' className={styles.submit}/>
-        </form>
+        <Landing />
+        <div style={{height:"400px",width:"100%",backgroundColor:"#ff3939",marginTop:"180px"}}></div>
       </div>
       )
   }
