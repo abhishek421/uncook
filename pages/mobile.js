7f4201937aa369f1bd0cfe111b35a8b19518efaf
Abhishek Choudhary <64063908+abhishek421@users.noreply.github.com>
2020-08-23 03:40:21 +0530

    New UI

---
+++ pages/mobile.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import styles from '../styles/Mobile.module.css';
+
+class Mobile extends React.Component{
+  render(){
+      return(
+        <div className={styles.background}>
+        <Head>
+          <title>UnCook</title>
+          <link rel="icon" href="/logo-r.svg" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+        </Head>
+        <div className={styles.navbar}>
+            <div className={styles.menu}></div>
+            <div className={styles.logo}></div>
+            <div className={styles.profile}></div>
+        </div>
+
+        <div className={styles.container}>
+
+        </div>
+        <div className={styles.tabs}>
+            
+        </div>
+      </div>
+      )
+  }
+}
+
+export default Mobile;
