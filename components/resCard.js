fc081b2903c20ac3c40d4885c468ed4806461773
Abhishek Choudhary <64063908+abhishek421@users.noreply.github.com>
2020-08-31 00:55:58 +0530

    Done

--- components/resCard.js
+++ components/resCard.js
@@ -1,23 +1,25 @@
 import Link from "next/link";
 import styles from '../styles/ResCard.module.css'
 
-function ResCard(props){
+class ResCard extends React.Component{
+    render(){
         return(
-            <Link href="/restaurant/[slug]" as={`/restaurant/${props.restro.restaurant.id}`}>
+            <Link href="/restaurant/[slug]" as={`/restaurant/${this.props.restro.restaurant.id}`}>
             <a className={styles.wrapper}>
-                <div className={styles.image} style={{backgroundImage:"url(" + props.restro.restaurant.thumb + ")"}}></div>
+                <div className={styles.image} style={{backgroundImage:"url(" + this.props.restro.restaurant.thumb + ")"}}></div>
                 <div className={styles.details}>
-                    <div className={styles.name}>{props.restro.restaurant.name}</div>
-                    <div className={styles.address}>{props.restro.restaurant.location.address}</div>
-                    <div className={styles.tags}>{props.restro.restaurant.cuisines}</div>
+                    <div className={styles.name}>{this.props.restro.restaurant.name}</div>
+                    <div className={styles.address}>{this.props.restro.restaurant.location.address}</div>
+                    <div className={styles.tags}>{this.props.restro.restaurant.cuisines}</div>
                     <div className={styles.info}>
-                        <div className={styles.rating}>{props.restro.restaurant.user_rating.aggregate_rating}</div>
-                        <div className={styles.reviews}>{props.restro.restaurant.user_rating.votes}</div>
+                        <div className={styles.rating}>{this.props.restro.restaurant.user_rating.aggregate_rating}</div>
+                        <div className={styles.reviews}>{this.props.restro.restaurant.user_rating.votes}</div>
                     </div>
                 </div>
             </a>
             </Link>
         )
     }
+}
 
 export default ResCard
